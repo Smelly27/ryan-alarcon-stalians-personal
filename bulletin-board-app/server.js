ef3ac943b6e038a294ed5965aee61efbd1d01c81
Smelly27 <ryan3ds@gmail.com>
2025-12-15 22:37:12 -0800

    Update README to trigger PR

--- bulletin-board-app/server.js
+++ bulletin-board-app/server.js
@@ -34,5 +34,9 @@ app.get('/api/events', api.events);
 app.post('/api/events', api.event);
 app.delete('/api/events/:eventId', api.event);
 
-app.listen(8080);
-console.log('Magic happens on port 8080...');
+if (require.main === module) {
+  app.listen(8080, () => {
+    console.log('Magic happens on port 8080...');
+  });
+}
+
